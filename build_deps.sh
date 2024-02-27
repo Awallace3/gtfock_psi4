716bee8f44742fae3fa7256d7a5def4de74aca8f
Austin Wallace <awallace43@gatech.edu>
2024-02-27 13:59:52 -0500

    submodule updates

--- build_deps.sh
+++ build_deps.sh
@@ -12,38 +12,58 @@ if [ ! -d simint-generator ]; then
     git submodule update --init --recursive
 fi
 
-cd simint-generator
-rm -r build
-mkdir build && cd build
-# CC=$CC CXX=$CXX cmake ../ -DCMAKE_CXX_FLAGS="-fPIC -I/usr/include/x86_64-linux-gnu -std=c++14"
-CC=$CC CXX=$CXX cmake .. -DCMAKE_CC_FLAGS="-fPIC -I/usr/include/x86_64-linux-gnu -std=c++14" -DCMAKE_CXX_FLAGS="-fPIC -I/usr/include/x86_64-linux-gnu -std=c++14"
-make -j16
-cd ..
-# exit script
-exit 0
+# if [ ! -d simint ]; then
+    cd simint-generator
+    rm -r build
+    mkdir build && cd build
+    CC=$CC CXX=$CXX cmake .. # -DCMAKE_CC_FLAGS="-fPIC -I/usr/include/x86_64-linux-gnu -std=c++14" -DCMAKE_CXX_FLAGS="-fPIC -I/usr/include/x86_64-linux-gnu -std=c++14"
+    make -j16
+    cd ..
 
-# Generate Simint source code (requires Python3)
-# Run ./create.py --help to see the details of the parameters
-./create.py -g build/generator/ostei -l 5 -p 4 -d 0 -ve 4 -vg 5 -he 4 -hg 5 simint
-rm -r ../simint
-mv simint ../
+    # Generate Simint source code (requires Python3)
+    # Run ./create.py --help to see the details of the parameters
+    ./create.py -g build/generator/ostei -l 5 -p 4 -d 0 -ve 4 -vg 5 -he 4 -hg 5 simint
+    rm -r ../simint
+    mv simint ../
 
-# Compile Simint
-cd ../simint  # Should at $WORK_TOP/simint
-# See the README file in Simint directory to see which SIMINT_VECTOR variable you should use
-# Commonly used SIMINT_VECTOR: commonavx512, avx2
+    # Compile Simint
+    cd ../simint  # Should at $WORK_TOP/simint
+    # See the README file in Simint directory to see which SIMINT_VECTOR variable you should use
+    # Commonly used SIMINT_VECTOR: commonavx512, avx2
 
-mkdir build-avx512 && cd build-avx512
-CC=$CC CXX=$CXX cmake ../ -DSIMINT_VECTOR=commonavx512 -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_CXX_FLAGS="-fPIC"
-make -j16 install
-cd ../..
-
+    mkdir build-avx512 && cd build-avx512
+    CC=$CC CXX=$CXX cmake ../ -DSIMINT_VECTOR=commonavx512 -DCMAKE_INSTALL_PREFIX=./install -DCMAKE_CXX_FLAGS="-fPIC"
+    make -j16 install
+    export SIMINT_LIBRARY_DIR=$PWD/install
+    cd ../..
+# fi
 
 cd libcint
-bash build.sh
+export objdir=objdir_cint
+rm -r $objdir
+rm -r include
+rm -r lib
+mkdir -p $objdir
+cmake -S. -B${objdir} -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_Fortran_COMPILER=ifort -DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH=${SIMINT_LIBRARY_DIR} -G Ninja -DCMAKE_INSTALL_PREFIX=.
+cd $objdir
+ninja install
+cd ..
 
 cd ../GTMatrix
-bash build.sh
+rm -r build
+rm -r include
+rm -r lib
+mkdir -p build && cd build
+cmake -G Ninja .. -DCMAKE_INSTALL_PREFIX=.. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DMPI_C_COMPILER=mpiicc -DMPI_CXX_COMPILER=mpiicpc
+ninja install
+cd ..
 
 cd ../GTFock
-bash build.sh
+rm -rf build
+mkdir -p build
+cd build
+
+export AR=xiar rcs
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCInt_DIR=/theoryfs2/ds/amwalla3/projects/gtf_psi4/libcint/share/cmake/CInt -DGTMatrix_DIR=/theoryfs2/ds/amwalla3/projects/gtf_psi4/GTMatrix/share/cmake/GTMatrix .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_Fortran_COMPILER=ifort -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
+make
+cd ..
